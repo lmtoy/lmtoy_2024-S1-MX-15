a0df02949a6d191c4436373af50f883e127a2e74
Peter Teuben <teuben@gmail.com>
2025-09-02 15:54:24 +0000

    many new obsnums

--- mk_runs.py
+++ mk_runs.py
@@ -1,29 +1,41 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
-
 import os
 import sys
 
 from lmtoy import runs
 
-project="2023-S1-US-00"
+project="2024-S1-MX-15"
 
-#        obsnums per source (make it negative if not added to the final combination)
+# Dictionary of sources, each with a list of obsnum's in this project
+# negative obsnums are ignored in the combinations. See also comments.txt
+# for obsnum specific comments and parameters!
 on = {}
-on['foo'] = [ ]
+
+on["WR140_CO"] = \
+ [ 139132, 139134, 139136, 139142, 139193, 139197, 139311, 139313, 139315, 139317, 139319, 139368, 139370,
+   139915,
+   139982, 139983, 139984, 139989, 140001, 140002, 140003, 140071, 140072, 140073, 140077, 140165, 140166, 140167,
+   140527,
+   140813,  140814, ]
 
 
-#        common parameters per source on the first dryrun (run1a, run2a)
+# parameters for the first pass of the pipeline (restart=1 is automatically enforced here)
 pars1 = {}
-pars1['foo']   = ""
 
-#        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
+pars1["WR140_CO"] = ""
+
+# parameters for the (optional) second pass of the pipeline (e.g. for bank=0)
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
 
-#        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
+pars2["WR140_CO"] = ""
+
+# parameters for the (optional) third pass of the pipeline (usually for bank=1)
 pars3 = {}
-pars3['foo']   = "srdp=1 admit=0"
 
-if __name__ == '__main__':    
+pars3["WR140_CO"] = ""
+
+# Found 1 source(s) for 2024-S1-MX-15
+
+if __name__ == "__main__":
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
